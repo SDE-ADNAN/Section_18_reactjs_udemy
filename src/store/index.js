60c1061704d279d1f8f36886112bc02ed755ba4d
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-01-29 00:26:50 +0530

    imported the createStore func from the redux lib instead of react-redux lib

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
-import {createStore} from "react-redux";
+import { createStore } from "redux";
+// import {createStore} from "redux"
 
 const counterReducer=(state={counter:0},action)=>{
 if(action.type==="increment"){
