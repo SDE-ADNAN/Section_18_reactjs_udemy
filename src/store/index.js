23f5d588761fba2b9b7355114c2d31ea24cd5ee7
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-01-30 23:50:39 +0530

    239 : Working with multiple state properties. < redux store logic  >

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,39 @@
 import { createStore } from "redux";
 // import {createStore} from "redux"
 
-const counterReducer=(state={counter:0},action)=>{
-if(action.type==="increment"){
+
+const initState = { counter: 0, showCounter: true };
+const counterReducer = (state = initState, action) => {
+  if (action.type === "increment") {
     return {
-        counter:state.counter+1,
-    }
-}
-if (action.type === "increase") {
-  return {
-    counter: state.counter + action.amount,
-  };
-}
-if(action.type==="decrement"){
+      ...state,
+      counter: state.counter + 1,
+      //   showCounter: state.showCounter,
+    };
+  }
+  if (action.type === "increase") {
     return {
-        counter:state.counter-1,
-    }
-}
-return state ;
-}
+      ...state,
+      counter: state.counter + action.amount,
+      //   showCounter: state.showCounter,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      ...state,
+      counter: state.counter - 1,
+      //   showCounter: state.showCounter,
+    };
+  }
+  if (action.type === "toggle") {
+    return {
+      ...state,
+      //   counter: state.counter,
+      showCounter: !state.showCounter,
+    };
+  }
+  return state;
+};
 
 const store = createStore(counterReducer);
 
