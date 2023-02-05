b7f374100ba5579037841465ec144639b155c2dc
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-02-05 23:57:07 +0530

    245 : Wroking with multiple slices < made use of the multi slice usecase of the redux toolkit store >

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,14 +3,14 @@ import { useDispatch, useSelector} from "react-redux";
 import { counterActions } from "../store/index";
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector((state) => state.counter);
-  const showCounter = useSelector((state) => state.showCounter);
+  const counter = useSelector((state) => state.counter.value);
+  const showCounter = useSelector((state) => state.counter.showCounter);
 
   const incrementHandler = () => {
     dispatch(counterActions.increment());
   };
   const increaseHandler = () => {
-    dispatch(counterActions.increase(5)); // here if we need to pass some extra data to the action creator 
+    dispatch(counterActions.increase(10)); // here if we need to pass some extra data to the action creator 
                                           // we pass it inside the calling paranthesis of the actions creator
                                           // {type:"auto generated unique identifier for increase", payload:5}
   };
@@ -29,7 +29,7 @@ const Counter = () => {
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={decrementHandler}>Decrement</button>
-        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={increaseHandler}>Increase by 10</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
@@ -38,57 +38,3 @@ const Counter = () => {
 
 export default Counter;
 
-// class Counter extends Component {
-//   incrementHandler() {
-//     this.props.increment();
-//   }
-//   decrementHandler() {
-//     this.props.decrement();
-//   }
-//   toggleCounterHandler() {}
-//   render() {
-//     return (
-//       <main className={classes.counter}>
-//         <h1>Redux Counter</h1>
-//         <div className={classes.value}>{this.props.counter}</div>
-//         <div>
-//           <button onClick={this.incrementHandler.bind(this)}>Increment</button>
-//           <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
-//         </div>
-//         <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
-//       </main>
-//     );
-//   }
-// }
-
-// // we can use the connect HOC in both of the components class as well as function based component.
-
-// // Over here we are using the connect for wrapping the Counter component with the function which the connect function returns.
-// // the reason why we use the connect like this is that connect requires two arguments , to be precise it requires two arguments which are basically functions namely ->
-// // 1.
-
-// const mapStateToProps = (state) => {
-//   /**
-//    * -what it does is
-//    * it gets the state which is the redux state and then maps it to the component props
-//    * so that the store state  could be used inside the component usind the this.prop way.
-//    *
-//    */
-//   return {
-//     counter: state.counter,
-//   };
-// };
-
-// const mapDispatchToProps = (dispatch) => {
-//   /**
-//    * it is equivallent to useDispatch()
-//    * what it does is it maps the dispatch functions needed by the store to manipulate its state
-//    * to the components state and inside the component we can use those props to dispatch actions for the store state update.
-//    */
-//   return {
-//     increment: () => dispatch({ type: "increment" }),
-//     decrement: () => dispatch({ type: "decrement" }),
-//   };
-// };
-
-// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
